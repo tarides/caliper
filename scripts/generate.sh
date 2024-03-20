f99fc4a9b482c7ba6ec688d9cbb3bf3186549915
Puneeth Chaganti <punchagan@muse-amuse.in>
2024-03-20 12:21:15 +0530

    Download current bench dump as a tarball if not locally available

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -5,9 +5,21 @@ set -euo pipefail
 
 HERE=$(dirname "$0")
 CB_DUMP=$(realpath "${HERE}/../cb-dump/")
+CACHE=$(realpath "${HERE}/../cache/")
+
+rm -rf "${CACHE}"
+mkdir -p "${CACHE}"
+
+cd "${HERE}/.." || exit
+
+# Download dump if it doesn't exist
+if [ ! -d "${CB_DUMP}" ];
+then
+    wget -c https://github.com/ocurrent/current-bench/files/14662859/cb-dump.tar.gz
+    tar xzf cb-dump.tar.gz
+fi
 
 # Generate HTML for the dumped data
-CACHE=$(realpath "${HERE}/../cache/")
 echo "Generating cache dir ..."
 for f in "${CB_DUMP}"/*.json;
 do
